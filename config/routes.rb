06ee5667f3fac67a387d04b83d24c032a031cbc9
Philipp Schaefer <pschaefer@posteo.net>
2021-06-12 23:40:29 +0200

    box; center

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # get '/stack', to: 'pages#stack', as: :stack
   get '/stack', to: 'pages#stack'
   get '/box', to: 'pages#box'
+  get '/center', to: 'pages#center'
 end
